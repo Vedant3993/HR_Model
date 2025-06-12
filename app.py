381a23754c46ade9a70e67a6983011b42a29d75b
Vedant3993 <vedantfulsoundar9@gmail.com>
2025-06-13 00:32:30 +0530

    Done

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import sklearn
+
 from utilis import binary_cleanup
 
 
 
-model= joblib.load("attrition_model.joblib")
+model= joblib.load("model.joblib")
 st.title("Employee Attrition Predication")
 
 with st.form("attrition_form"):
